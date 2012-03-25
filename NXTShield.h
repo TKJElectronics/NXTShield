c455cb6bfb004cfe77cc585aa4836d4a0d231342
Kristian Lauszus <lauszus@gmail.com>
2012-03-25 15:57:57 +0200

    Updated header

--- NXTShield.h
+++ NXTShield.h
@@ -1,8 +1,18 @@
-/*
- * NXTShield library
- * Version 1.0 March, 2012
- * Copyright 2012 Kristian Lauszus, TKJ Electronics
- * For details, see http://blog.tkjelectronics.dk/
+/* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
+ 
+ This software may be distributed and modified under the terms of the GNU
+ General Public License version 2 (GPL2) as published by the Free Software
+ Foundation and appearing in the file GPL2.TXT included in the packaging of
+ this file. Please note that GPL2 Section 2[b] requires that all works based
+ on this software must also be made publicly available under the terms of
+ the GPL2 ("Copyleft").
+ 
+ Contact information
+ -------------------
+ 
+ Kristian Lauszus, TKJ Electronics
+ Web      :  http://www.tkjelectronics.com
+ e-mail   :  kristianl@tkjelectronics.com
  */
 
 #ifndef NXTShield_h
